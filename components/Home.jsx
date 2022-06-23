b3900586e3738eb66bfb6ab634d351e5d5d80842
Yassar021 <yassar.tkj114@gmail.com>
2022-06-23 17:43:50 +0700

    doneAllResposiveExceptBurgerMenus

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,13 +1,17 @@
-import { Box, Flex, Text,Show, Stack,Center,SimpleGrid,Button,Container,Link, Image, HStack } from "@chakra-ui/react"
+import { Box, Flex, Text,Show, Stack,Center,SimpleGrid,Button,Container,Link, Image, HStack, Divider, VStack } from "@chakra-ui/react"
 
 const Home = () => {
     return (
         <>
+        <Box>
         <Container maxW='8xl'>
             {/* Navbar  */}
             <Box pt='44px' px={{lg:'20px',xl:'149px'}}>
                 <Flex direction={'row'} justifyContent='space-between'>
-                    <Image src='/logo.png' w='40px' h='40px' alt='logo' />
+                    <HStack spacing='20px'>
+                        <Image src='/logo.png' w='40px' h='40px' alt='logo' />
+                        <Text fontSize={'24px'} fontWeight={'700'} color='#373737'>Masmas</Text>
+                    </HStack>
                     <Show above='md'>
                         <HStack spacing='63px'>
                             <Text fontSize={'15px'} fontWeight={'500'} color='#373737'>Home</Text>
@@ -21,7 +25,7 @@ const Home = () => {
             <Box pt='116px' pl={{lg:'20px',xl:'149px'}}>
                 {/* Hero */}
                 <Flex direction={{base:'column',lg:'row'}} textAlign={{base:'center',lg:'left'}}>
-                    <Box mx={{base:'auto',md:'0px'}} w={{base:'240px',md:'632px'}}>
+                    <Box mx={{base:'auto',md:'auto',lg:'0px'}} w={{base:'240px',md:'632px'}}>
                         <Text fontSize={'63px'} fontWeight={'900'} color='#373737'>We Help to Build Your Dream</Text>
                         <Text fontSize={'22px'} fontWeight={'500'} color='#5B5B5B' lineHeight={'36px'}>
                             Probabo, inquit, sic agam, ut summum malum et, quantum possit, a sapiente delectus. At magnum periculum adiit in quo minus id, quod maxime placeat, facere nondum.
@@ -45,12 +49,12 @@ const Home = () => {
                             >
                             Get Started
                         </Button>
-                        <Text mt='110px' fontSize={'18px'} fontWeight={'500'} color='#5B5B5B' >
+                        <Text mt={{base:'20px',md:'110px'}} fontSize={'18px'} fontWeight={'500'} color='#5B5B5B' >
                             Save 20% - 30% of from traditional cost
                         </Text>
                     </Box>
-                    <Box>
-                        <Image src='/hero.png' w='100%' h={{base:'auto',md:'900px'}} alt='hero-image' />
+                    <Box mt={{base:'60px',md:'0px'}}>
+                        <Image src='/hero.png' w={{lg:'623px'}} h={{base:'auto',md:'auto',lg:'900px'}} alt='hero-image' />
                     </Box>
                 </Flex>
 
@@ -60,16 +64,16 @@ const Home = () => {
                         <Text fontSize={'41px'} lineHeight='62px' fontWeight={'700'} color='#373737'>Meet New People in Your Community</Text>
                         <Text mt='10px' fontSize={'22px'} lineHeight='40px' fontWeight={'500'} color='#5b5b5b'>Connect with people of Faith locally and around the globe. Find other like-minded believers, make new friends, form deeper relationship & partnership, socialize and do more.</Text>
                     </Box>
-                    <Box>
-                        <Image src='/meet.png' w='100%' h={{lg:'auto',xl:'888px'}} alt='meet-people' />
+                    <Box mt={{base:'60px',md:'0px'}}>
+                        <Image src='/meet.png' w={{lg:'623px'}} h={{lg:'auto',xl:'888px'}} alt='meet-people' />
                     </Box>
                 </Flex>
             </Box>
             {/* Engage */}
             <Box pt='101px' bgColor={'#FAFAFA'}>
                 <Flex direction={{base:'column-reverse',md:'row'}} mt='101px' textAlign={{base:'center',md:'left'}}>                    
-                    <Box>
-                        <Image src='/engage.png' w='100%' h={{lg:'auto',xl:'888px'}} alt='meet-people' />
+                    <Box mt={{base:'60px',md:'0px'}}>
+                        <Image src='/engage.png' w={{lg:'623px'}} h={{lg:'auto',xl:'888px'}} alt='meet-people' />
                     </Box>
                     <Box w={{lg:'595px'}} my='auto'>
                         <Text fontSize={'41px'} fontWeight={'700'} lineHeight='62px' color='#373737'>Engage with Your Masmas Community</Text>
@@ -85,8 +89,8 @@ const Home = () => {
                         <Text fontSize={'41px'} lineHeight='62px' fontWeight={'700'} color='#373737'>Easy Donations to Your Church</Text>
                         <Text mt='10px' fontSize={'22px'} lineHeight='40px' fontWeight={'500'} color='#5b5b5b'>Its now so simple and seamless to give to your Church during service or whilst abroad. Donations take less than 10 secs. Simply review your past donations anytime, anywhere via the app. Masmas app also shows you the tax incentives you are entitled on your donations.</Text>
                     </Box>
-                    <Box>
-                        <Image src='/donation.png' w='100%' h={{lg:'auto',xl:'888px'}} alt='meet-people' />
+                    <Box mt={{base:'60px',md:'0px'}}>
+                        <Image src='/donation.png' w={{lg:'623px'}} h={{lg:'auto',xl:'888px'}} alt='meet-people' />
                     </Box>
                 </Flex>
             </Container>
@@ -94,8 +98,8 @@ const Home = () => {
             {/* attend */}
             <Box pt='101px' bgColor={'#FAFAFA'}>
                 <Flex direction={{base:'column-reverse',md:'row'}} mt='101px' textAlign={{base:'center',md:'left'}}>                    
-                    <Box>
-                        <Image src='/attend.png' w='100%' h={{lg:'auto',xl:'888px'}} alt='meet-people' />
+                    <Box mt={{base:'60px',md:'0px'}}>
+                        <Image src='/attend.png' w={{lg:'623px'}} h={{lg:'auto',xl:'888px'}} alt='meet-people' />
                     </Box>
                     <Box w={{lg:'595px'}} my='auto'>
                         <Text fontSize={'41px'} fontWeight={'700'} lineHeight='62px' color='#373737'>Attend Key Events You Care about</Text>
@@ -115,7 +119,7 @@ const Home = () => {
                         </Center>
                         <Box>
                             <Center>
-                                <Image mt='68px' src='/download.png' maxW={{base:'340px',md:'557px'}} h={{lg:'auto',xl:'554px','2xl':'554px'}} alt='download' />
+                                <Image mt='60px' src='/download.png' maxW={{base:'278px',md:'557px'}} h={{lg:'auto',xl:'554px','2xl':'554px'}} alt='download' />
                             </Center>
                         </Box>
                         <Box>
@@ -139,11 +143,11 @@ const Home = () => {
 
             {/* testimonials */}
             <Box pt='100px' pb='135px' px={{lg:'auto',xl:'160px'}} bgColor={'#FAFAFA'}>
-                <Center>
+                <Center textAlign={'center'}>
                     <Text fontSize={'37px'} fontWeight={'700'} color='#373737'>Members are loving Masmas</Text>
                 </Center>
-                <SimpleGrid mt='101px' columns={{md:2,lg:3}} spacing='58px'>
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(96, 114, 226, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                <SimpleGrid mt='101px' columns={{md:2,lg:3}} spacing='58px' mx={{base:'auto'}}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(96, 114, 226, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -153,7 +157,7 @@ const Home = () => {
                             </Box>
                         </HStack>
                     </Box>
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(152, 43, 190, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(152, 43, 190, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -163,7 +167,7 @@ const Home = () => {
                             </Box>
                         </HStack>
                     </Box>
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(226, 98, 98, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(226, 98, 98, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -173,7 +177,7 @@ const Home = () => {
                             </Box>
                         </HStack>
                     </Box> 
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(70, 194, 125, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(70, 194, 125, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -183,7 +187,7 @@ const Home = () => {
                             </Box>
                         </HStack>
                     </Box> 
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(67, 77, 144, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(67, 77, 144, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -193,7 +197,7 @@ const Home = () => {
                             </Box>
                         </HStack>
                     </Box> 
-                    <Box py='22px' px='20px' w='331px' bgColor={'rgba(231, 153, 65, 0.128196)'} borderRadius='11px' shadow={'sm'}>
+                    <Box py='22px' px='20px' w={{base:'290px',md:'331px'}} bgColor={'rgba(231, 153, 65, 0.128196)'} borderRadius='11px' shadow={'sm'}>
                         <Text fontSize={'16px'} fontWeight='500' color={'#37474F'} lineHeight='26px'>Vel eum dolor aut laudantium rerum quia eos et. Voluptas occaecati delectus eius. Nam aut temporibus dolorem quae et soluta. Necessitatibus voluptatem id atque veniam.</Text>
                         <HStack mt='58px' spacing={'22px'}>
                             <Image src='/profile.png' w='53px' h='53px' alt="profile" />
@@ -205,9 +209,93 @@ const Home = () => {
                     </Box>                    
                 </SimpleGrid>
             </Box>
-
+            
+            {/* Footer */}
+            <Flex pt='64px' direction={'column'} >
+                <Stack direction='column' mx='auto' spacing='20px' textAlign={'center'}>
+                    <Text fontSize={'23px'} fontWeight='500' color='#373737'>It’s Time</Text>
+                    <Text fontSize={'37px'} fontWeight='700' color='#373737'>Start meeting amazing people of Masmas locally and globally</Text>
+                    <Center>
+                        <Stack direction={{base:'column',md:'row'}} mt='74px' spacing={'22px'}>
+                                <Link>
+                                    <Image h='67px' w='100%' src='/app-store.png' alt='appstore' />
+                                </Link>
+                                <Link>
+                                    <Image h='67px' w='100%' src='/google-play.png' alt='googleplay' />
+                                </Link>
+                            </Stack>
+                    </Center>
+                </Stack>
+                <Divider mt='123px' />
+                <Flex direction={{base:'column',md:'column',xl:'row'}} mx={{base:'auto',md:'0px'}} px={{md:'20px',xl:'120px'}} pt={'54px'} textAlign={{base:'center', md:'center'}} justifyContent='space-between'>
+                    <HStack mx={{md:'auto',lg:'0px'}} spacing='20px'>
+                        <Image src='/logo.png' w='40px' h='40px' alt='logo' />
+                        <Text fontSize={'24px'} fontWeight={'700'} color='#373737'>Masmas</Text>
+                    </HStack>
+                    <Stack mt={{base:'60px',md:'40px',lg:'0px'}} direction={{base:'column',md:'row'}} spacing={{base:'60px',md:'120px'}}>
+                        <Box>
+                            <Text fontSize={'25px'} fontWeight={'600'} color='#2F281E'>Quick Links</Text>
+                            <VStack spacing='20px' mt='35px'>
+                                <Link>
+                                    <Text color='#544837' fontSize={'18px'}>About Us</Text>
+                                </Link>
+                                <Link>
+                                    <Text color='#544837' fontSize={'18px'}>Blog</Text>
+                                </Link>
+                                <Link>
+                                    <Text color='#544837' fontSize={'18px'}>Contact</Text>
+                                </Link>
+                                <Link>
+                                    <Text color='#544837' fontSize={'18px'}>FAQ</Text>
+                                </Link>
+                            </VStack>                       
+                        </Box>
+                        <Box>
+                            <Text fontSize={'25px'} fontWeight={'600'} color='#2F281E'>Legal Stuff</Text>
+                            <VStack spacing='20px' mt='35px'>
+                                    <Link>
+                                        <Text color='#544837' fontSize={'18px'}>Disclaimer</Text>
+                                    </Link>
+                                    <Link>
+                                        <Text color='#544837' fontSize={'18px'}>Terms of Service</Text>
+                                    </Link>
+                                    <Link>
+                                        <Text color='#544837' fontSize={'18px'}>Financing</Text>
+                                    </Link>
+                                    <Link>
+                                        <Text color='#544837' fontSize={'18px'}>Privacy Policy</Text>
+                                    </Link>
+                            </VStack>
+                        </Box>
+                        <Box>
+                            <Text fontSize={'25px'} fontWeight={'600'} color='#2F281E'>Social</Text>
+                            <VStack spacing='20px' mt='35px'>
+                                    <Link>
+                                        <HStack spacing='14px'>
+                                            <Image src='/fb.png' w='10px' h='16px' alt='facebook' />
+                                            <Text fontSize={'18px'} color='#544837'>Facbook</Text>
+                                        </HStack>
+                                    </Link>
+                                    <Link>
+                                        <HStack spacing='14px'>
+                                            <Image src='/twitter.png' w='16px' h='14px' alt='twitter' />
+                                            <Text fontSize={'18px'} color='#544837'>Twitter</Text>
+                                        </HStack>
+                                    </Link>
+                                    <Link>
+                                        <HStack spacing='14px'>
+                                            <Image src='/ig.png' w='16px' h='16px' alt='instagram' />
+                                            <Text fontSize={'18px'} color='#544837'>Instagram</Text>
+                                        </HStack>
+                                    </Link>
+                            </VStack>
+                        </Box>
+                    </Stack>
+                </Flex>
+            </Flex>
 
         </Container>
+        </Box>
     </>
     )
 }
